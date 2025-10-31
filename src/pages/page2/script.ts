ff69a5ed9b4c7238df631864bf8e88e2e6485dcc
_0w0_ <wang124yi@163.com>
2025-10-31 15:27:53 +0800

    tabs

--- src/pages/page2/script.ts
+++ src/pages/page2/script.ts
@@ -1,5 +1,6 @@
 import "../../shared/style.css";
 import { sharedFunction } from "../../shared/utils";
+import { Tabs } from "./tabs";
 
 sharedFunction(); // 這是共享的程式碼。
 
@@ -9,3 +10,7 @@ if (language === "zh") {
 } else if (language === "en") {
   console.log("This is the English page");
 }
+
+window.onload = function () {
+  new Tabs();
+};
---
+++ src/pages/page2/tabs.ts
@@ -0,0 +1,95 @@
+// TODO: 存在BUG，当图片宽度大于容器一点时，最右侧有穿帮
+export class Tabs {
+
+
+    constructor() {
+        const tabs = document.querySelectorAll('div[type="tabs"]');
+        for (let i = 0; i < tabs.length; i++) {
+            const tc = tabs[i];
+            this.initTabs(tc as HTMLDivElement);
+        }
+    }
+
+    initTabs(tabs: HTMLDivElement) {
+        const tabsViews = tabs.querySelectorAll('div[type="tabsView"]');
+        for (let i = 0; i < tabsViews.length; i++) {
+            const tabsV = tabsViews[i] as HTMLDivElement;
+            tabsV.style.width = `${tabs.clientWidth}px`;
+            console.log('tabsV:', tabsV, tabsV.clientWidth);
+            this.initTabView(tabsV as HTMLDivElement);
+        }
+
+        const btnGroup = tabs.querySelectorAll('div[type="btnGroup"]');
+        for (let i = 0; i < btnGroup.length; i++) {
+            const btnG = btnGroup[i];
+            this.initBtnGroupItems(btnG as HTMLDivElement);
+        }
+    }
+
+
+    initBtnGroupItems(btnGroup: HTMLDivElement) {
+        const btnGroupItems = btnGroup.querySelectorAll('div[type="btnGroupItem"]');
+        var select = btnGroup.querySelector('.select') as HTMLSpanElement;
+        if (select == null) {
+            select = document.createElement('span');
+            btnGroup.appendChild(select);
+        }
+        for (let i = 0; i < btnGroupItems.length; i++) {
+            const btnGItem = btnGroupItems[i];
+            const selected = btnGItem.getAttribute('select');
+            if (selected != null) {
+                select.style.left = (btnGItem as HTMLDivElement).offsetLeft + "px";
+                const sto = setTimeout(() => {
+                    select.style.transitionDuration = "300ms";
+                    clearTimeout(sto);
+                }, 10);
+
+
+                // 获取按钮设置的tabID
+                const tabID = btnGItem.getAttribute('tabID');
+                // 获取tabID对应的tab
+                const tabDiv = document.querySelector(`div[type="tab"][tabID="${tabID}"]`) as HTMLDivElement;
+                const tabsView = tabDiv.parentElement as HTMLDivElement;
+                tabsView.style.left = `-${tabDiv.offsetLeft}px`;
+                tabsView.style.transitionDuration = "300ms";
+            }
+            btnGItem.addEventListener('click', (event) => {
+                const thisBtn = event.target as HTMLDivElement;
+                const selected = thisBtn.getAttribute('select');
+                if (selected != null) {
+                    return;
+                }
+                // console.log('thisBtn:', thisBtn.offsetLeft);
+                // 移除所有选中项
+                for (let i = 0; i < btnGroupItems.length; i++) {
+                    const btnGItem = btnGroupItems[i];
+                    btnGItem.removeAttribute('select');
+                }
+                // 添加选中项
+                thisBtn.setAttribute('select', '');
+                select.style.left = thisBtn.offsetLeft + 'px';
+
+
+                // 获取按钮设置的tabID
+                const tabID = thisBtn.getAttribute('tabID');
+                // 获取tabID对应的tab
+                const tabDiv = document.querySelector(`div[type="tab"][tabID="${tabID}"]`) as HTMLDivElement;
+                const tabsView = tabDiv.parentElement as HTMLDivElement;
+                tabsView.style.left = `-${tabDiv.offsetLeft}px`;
+
+                // console.log('tabID:', tabID, tabDiv, tabDiv.offsetLeft, tabsView);
+            });
+        }
+    }
+
+    initTabView(tabsView: HTMLDivElement) {
+        const tabs = tabsView.querySelectorAll('div[type="tab"]');
+        // console.log('tabsView:', tabsView, tabsView.clientWidth);
+        for (let i = 0; i < tabs.length; i++) {
+            const tab = tabs[i] as HTMLDivElement;
+            tab.style.width = `${tabsView.clientWidth}px`
+        }
+        tabsView.style.width = `${tabs.length * tabsView.clientWidth}px`;
+    }
+}
+
