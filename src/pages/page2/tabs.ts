3cfc4fba63ff5a820b4b038b76a4cc4a1e82fe68
_0w0_ <wang124yi@163.com>
2025-11-07 11:31:09 +0800

    添加切换换页效果

--- src/pages/page2/tabs.ts
+++ src/pages/page2/tabs.ts
@@ -18,7 +18,11 @@ export class Tabs {
         if (tabsDirection != null) {
             direction = tabsDirection as "Vertical" | "Horizontal";
         }
-        console.log('initTabs direction:', direction);
+        var animation: "row" | "switch" = "row";
+        const tabAnimation = tabs.getAttribute('animation');
+        if (tabAnimation != null) {
+            animation = tabAnimation as "row" | "switch";
+        }
         // 获取Tabs场景容器
         const tabsViews = tabs.querySelectorAll('[type="tabsView"]');
         for (let i = 0; i < tabsViews.length; i++) {
@@ -27,20 +31,20 @@ export class Tabs {
             } else {
                 tabsV.style.width = `${tabs.clientWidth}px`;
             }
-            this.initTabView(tabsV as HTMLDivElement, direction);
+            this.initTabView(tabsV as HTMLDivElement, direction, animation);
         }
 
         // 获取按钮组
         const btnGroup = tabs.querySelectorAll('[type="btnGroup"]');
         for (let i = 0; i < btnGroup.length; i++) {
             const btnG = btnGroup[i];
-            this.initBtnGroupItems(btnG as HTMLDivElement, direction);
+            this.initBtnGroupItems(btnG as HTMLDivElement);
         }
     }
 
 
     // 初始化按钮组中的按钮
-    initBtnGroupItems(btnGroup: HTMLDivElement, direction: "Vertical" | "Horizontal") {
+    initBtnGroupItems(btnGroup: HTMLDivElement) {
         const btnGroupItems = btnGroup.querySelectorAll('[type="btnGroupItem"]');
         // 获取选中标记
         var select = btnGroup.querySelector('.select') as HTMLSpanElement;
@@ -54,6 +58,7 @@ export class Tabs {
             const btnGItem = btnGroupItems[i];
             // 获取按钮的选中状态 为 null 则没有，为 空字符串 则有select属性当前按钮为按下状态
 
+            var animation: "row" | "switch" = "row";
             var direction: "Vertical" | "Horizontal" = "Vertical";
             const tabsRoot = btnGroup.parentElement as HTMLDivElement;
             if (tabsRoot) {
@@ -61,17 +66,31 @@ export class Tabs {
                 if (tabsDirection != null) {
                     direction = tabsDirection as "Vertical" | "Horizontal";
                 }
+                const tabAnimation = tabsRoot.getAttribute('animation');
+                if (tabAnimation != null) {
+                    animation = tabAnimation as "row" | "switch";
+                }
             }
+            const btnTitle = btnGItem.querySelector('[title]') as HTMLElement;
+            const btnContent = btnGItem.querySelector('[content]') as HTMLElement;
 
             const selected = btnGItem.getAttribute('select');
             if (selected != null) {
-                // 根据选中状态设置select的left值
-                if (direction == "Horizontal") {
-                    // 根据选中状态设置select的left值
-                    select.style.top = (btnGItem as HTMLDivElement).offsetTop + "px";
-                } else {
-                    select.style.left = (btnGItem as HTMLDivElement).offsetLeft + "px";
+                if (btnTitle && btnContent) {
+                    btnTitle.classList.add('display-none')
+                    btnContent.classList.remove('display-none');
                 }
+                // 等待一下再设置select的left值，因为选中按钮需要根据隐藏的项重新排版
+                setTimeout(() => {
+                    // 根据选中状态设置select的left值
+                    if (direction == "Horizontal") {
+                        // 根据选中状态设置select的left值
+                        select.style.top = (btnGItem as HTMLDivElement).offsetTop + "px";
+                        select.style.height = (btnGItem as HTMLDivElement).offsetHeight + "px";
+                    } else {
+                        select.style.left = (btnGItem as HTMLDivElement).offsetLeft + "px";
+                    }
+                }, 10);
                 // 延迟一下在添加过度动画时间，以免从屏幕最左侧移动到选中按钮下，如需要此动画去掉setTimeout
                 const sto = setTimeout(() => {
                     select.style.transitionDuration = "300ms";
@@ -82,33 +101,70 @@ export class Tabs {
                 // 获取按钮设置的tabID
                 const tabID = btnGItem.getAttribute('tabID');
                 // 获取tabID对应的tab
-                const tabDiv = document.querySelector(`[type="tab"][tabID="${tabID}"]`) as HTMLDivElement;
+                const tabDiv = tabsRoot.querySelector(`[type="tab"][tabID="${tabID}"]`) as HTMLDivElement;
                 const tabsView = tabDiv.parentElement as HTMLDivElement;
-                if (direction == "Horizontal") {
-                    tabsView.style.top = `-${tabDiv.offsetTop}px`;
-                } else {
-                    tabsView.style.left = `-${tabDiv.offsetLeft}px`;
+                if (animation == "row") {
+                    if (direction == "Horizontal") {
+                        tabsView.style.top = `-${tabDiv.offsetTop}px`;
+                    } else {
+                        tabsView.style.left = `-${tabDiv.offsetLeft}px`;
+                    }
+                } else if (animation == "switch") {
+                    const allTabDiv = tabsRoot.querySelectorAll(`[type="tab"]`);
+                    for (let i = 0; i < allTabDiv.length; i++) {
+                        const td = allTabDiv[i] as HTMLDivElement;
+                        if (td == tabDiv) {
+                            td.classList.add('select-tab');
+                            td.classList.remove('display-none');
+                            continue
+                        }
+                        td.classList.remove('select-tab');
+                        td.classList.add('display-none');
+                    }
                 }
                 tabsView.style.transitionDuration = "300ms";
+            } else if (btnTitle && btnContent) {
+                btnTitle.classList.remove('display-none');
+                btnContent.classList.add('display-none');
             }
+
 
             // 添加点击事件
             btnGItem.addEventListener('click', (event) => {
-                const thisBtn = event.target as HTMLDivElement;
+                const thisBtn = event.target === event.currentTarget ? event.target as HTMLDivElement : (event.target as HTMLDivElement).parentElement as HTMLDivElement;
                 const selected = thisBtn.getAttribute('select');
                 if (selected != null) {
                     return;
                 }
+
+
                 // 移除所有选中项
                 for (let i = 0; i < btnGroupItems.length; i++) {
                     const btnGItem = btnGroupItems[i];
+                    if (btnGItem == thisBtn) {
+                        continue;
+                    }
                     btnGItem.removeAttribute('select');
+                    const btnTitle = btnGItem.querySelector('[title]') as HTMLElement;
+                    const btnContent = btnGItem.querySelector('[content]') as HTMLElement;
+                    if (btnTitle && btnContent) {
+                        btnTitle.classList.remove('display-none');
+                        btnContent.classList.add('display-none');
+                    }
+
+                }
+                const btnTitle = thisBtn.querySelector('[title]') as HTMLElement;
+                const btnContent = thisBtn.querySelector('[content]') as HTMLElement;
+                if (btnTitle && btnContent) {
+                    btnTitle.classList.add('display-none')
+                    btnContent.classList.remove('display-none');
                 }
                 // 添加选中项
-                thisBtn.setAttribute('select', '');
+
                 if (direction == "Horizontal") {
                     // 根据选中状态设置select的left值
                     select.style.top = thisBtn.offsetTop + "px";
+                    select.style.height = (btnGItem as HTMLDivElement).offsetHeight + "px";
                 } else {
                     select.style.left = thisBtn.offsetLeft + 'px';
                 }
@@ -118,29 +174,58 @@ export class Tabs {
                 // 获取tabID对应的tab
                 const tabDiv = document.querySelector(`[type="tab"][tabID="${tabID}"]`) as HTMLDivElement;
                 const tabsView = tabDiv.parentElement as HTMLDivElement;
-                if (direction == "Horizontal") {
-                    tabsView.style.top = `-${tabDiv.offsetTop}px`;
-                } else {
-                    tabsView.style.left = `-${tabDiv.offsetLeft}px`;
+                if (animation == "row") {
+                    if (direction == "Horizontal") {
+                        tabsView.style.top = `-${tabDiv.offsetTop}px`;
+                    } else {
+                        tabsView.style.left = `-${tabDiv.offsetLeft}px`;
+                    }
+                } else if (animation == "switch") {
+                    const allTabDiv = tabsRoot.querySelectorAll(`[type="tab"]`);
+
+                    tabDiv.classList.remove('display-none');
+                    setTimeout(() => {
+                        tabDiv.classList.add('select-tab');
+                        for (let i = 0; i < allTabDiv.length; i++) {
+                            const td = allTabDiv[i] as HTMLDivElement;
+                            if (td == tabDiv) {
+                                continue
+                            }
+                            td.classList.remove('select-tab');
+                        }
+                    }, 10);
+                    setTimeout(() => {
+                        for (let i = 0; i < allTabDiv.length; i++) {
+                            const td = allTabDiv[i] as HTMLDivElement;
+                            if (td == tabDiv) {
+                                continue
+                            }
+                            td.classList.add('display-none');
+                        }
+                    }, 1000);
                 }
+                // 延迟一下再添加，上面的动画需要先准备一下
+                setTimeout(() => {
+                    thisBtn.setAttribute('select', '');
+                }, 10);
             });
         }
     }
 
     // 初始化Tab场景
-    initTabView(tabsView: HTMLDivElement, direction: "Vertical" | "Horizontal") {
+    initTabView(tabsView: HTMLDivElement, direction: "Vertical" | "Horizontal", animation: "row" | "switch") {
         const tabs = tabsView.querySelectorAll('[type="tab"]');
-        console.log('initTabView direction:', direction,);
         for (let i = 0; i < tabs.length; i++) {
             const tab = tabs[i] as HTMLDivElement;
-            if (direction == "Horizontal") {
-                // tab.style.width = `${tabsView.clientWidth}px`
-            } else {
+            console.log('initTabView switch:', animation, tab);
+            if (animation == "row" && direction == "Vertical") {
                 tab.style.width = `${tabsView.clientWidth}px`
+                // } else if (animation == "switch") {
+                //     tab.classList.add('display-none');
             }
         }
-        if (direction == "Horizontal") {
-        } else {
+        const animationType = tabsView.getAttribute('animation');
+        if (direction == "Vertical" && animationType == "row") {
             tabsView.style.width = `${tabs.length * tabsView.clientWidth}px`;
         }
     }
